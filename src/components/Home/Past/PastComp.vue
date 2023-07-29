1ffe1b3d5894ec50898e4c4c79cd1bd50225456d
Toxirrrr <mjuraeva74@gmail.com>
2023-07-29 15:30:27 +0500

    Toxirrrr

--- src/components/Home/Past/PastComp.vue
+++ src/components/Home/Past/PastComp.vue
@@ -3,16 +3,18 @@ import './PastComp.css'
 </script>
 
 <template>
-    <section class="past">
+  <section class="past">
     <div class="container">
-        <div class="past__wrapper">
-            <img class="past__img" src="/img/past__logo.svg" alt="past__logo">
-            <p class="past__text">After the events at the Leipzig-Halle airport, Natasha was overtaken by the mistakes of the past. The Red Room again made itself felt - a program to create Widows - professional killers and spies - just like her. Natasha decides once and for all to deal with people whose efforts she herself became a Widow.
-            </p>
-            <button class="past__btn">
-                Watch now
-            </button>
-        </div>
-    </div>        
-    </section>
+      <div class="past__wrapper">
+        <img class="past__img" src="/img/past__logo.svg" alt="past__logo" />
+        <p class="past__text">
+          After the events at the Leipzig-Halle airport, Natasha was overtaken by the mistakes of
+          the past. The Red Room again made itself felt - a program to create Widows - professional
+          killers and spies - just like her. Natasha decides once and for all to deal with people
+          whose efforts she herself became a Widow.
+        </p>
+        <button class="past__btn">Watch now</button>
+      </div>
+    </div>
+  </section>
 </template>
