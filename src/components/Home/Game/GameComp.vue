b3bf3334a506d410d27e498f7b87ab26570313e6
Toxirrrr <mjuraeva74@gmail.com>
2023-07-31 12:53:33 +0500

    game section created

---
+++ src/components/Home/Game/GameComp.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import './GameComp.css'
+</script>
+
+
+<template>
+    <section class="game">
+        <div class="container">
+            <div class="game__wrapper">
+                <h2 class="game__header">Game of Thrones</h2>
+                <p class="game__text">
+                    The time of prosperity is drawing to a close, and the summer, which lasted almost a decade, is dying away. Around the center of power of the Seven Kingdoms, the Iron Throne, a conspiracy is ripening, and during this difficult time, the king decides to seek support from his youth friend Eddard Stark. In a world where everyone - from the king to the mercenary - is eager for power, intrigues and is ready to stick a knife in the back, there is ...
+                </p>
+
+                <button class="game__btn">
+                    Watch now
+                </button>
+            </div>
+        </div>
+    </section>
+</template>
